1c4ae300321a98e1af5a2d4e64966ce101c6d881
rinasaosrm <113373072+rinasaosrm@users.noreply.github.com>
2022-10-18 17:27:03 +0530

    Create throttling.js
    throttle

---
+++ throttling.js
@@ -0,0 +1,29 @@
+const loggerFunc = () => {
+  console.count("Throttled Function");
+}
+
+const throttle = (fn, limit) => {
+  let flag = true;
+  return function(){
+    let context = this;
+    let args = arguments;
+    if(flag){
+      fn.apply(context, args);
+      flag = false;
+      setTimeout(() => {
+        flag=true;
+      }, limit);
+    }
+  }
+}
+
+const betterLoggerFunction = throttle(loggerFunc, 1000);
+
+window.addEventListener("resize",betterLoggerFunction);
+
+const normalFunc = () => {
+  console.count("Normal Function");
+}
+
+window.addEventListener("resize",normalFunc);
+
